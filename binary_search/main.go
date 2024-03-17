467bebd9601652bbfd34e7902cf3f1fce78b57fd
Oleksandr Vlasov <drumlife182@gmail.com>
2024-03-17 18:39:15 +0100

    Binary Search deeper

--- binary_search/main.go
+++ binary_search/main.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func checkBin(list []int, i int) int {
+	// min border of my numbers
 	low := 0
+
+	// Last number in the array because the slice start from 0...n
 	high := len(list) - 1
+
+	// If the guess less than or equal of our nunbers
 	for low <= high {
 		mid := (low + high) / 2
 		if list[mid] == i {
@@ -20,6 +25,7 @@ func checkBin(list []int, i int) int {
 }
 
 func main() {
-	fmt.Println(checkBin([]int{1, 2, 3, 4, 5}, 1))
-	fmt.Println(checkBin([]int{1, 2, 3, 4, 5}, -1))
+	mylist := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	fmt.Println(checkBin(mylist, 17)) //
+	fmt.Println(checkBin(mylist, -1)) // nil
 }
